12c9742253ac8f0cb1fc4c7ef67ea2d98987821a
Linh <thuylinhngo773@gmail.com>
2024-03-11 23:59:25 +0700

    l

--- SimpleBusTicket-PHP/SimpleBusTicket-PHP/DATABASE FILE/sbtbsphp.sql
+++ SimpleBusTicket-PHP/SimpleBusTicket-PHP/DATABASE FILE/sbtbsphp.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 06, 2024 at 07:33 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 11, 2024 lúc 05:46 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sbtbsphp`
+-- Cơ sở dữ liệu: `sbtbsphp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `alogin`
+-- Cấu trúc bảng cho bảng `alogin`
 --
 
 CREATE TABLE `alogin` (
@@ -34,7 +34,7 @@ CREATE TABLE `alogin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `alogin`
+-- Đang đổ dữ liệu cho bảng `alogin`
 --
 
 INSERT INTO `alogin` (`id`, `email`, `password`) VALUES
@@ -43,7 +43,7 @@ INSERT INTO `alogin` (`id`, `email`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookings`
+-- Cấu trúc bảng cho bảng `bookings`
 --
 
 CREATE TABLE `bookings` (
@@ -58,23 +58,24 @@ CREATE TABLE `bookings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `bookings`
+-- Đang đổ dữ liệu cho bảng `bookings`
 --
 
 INSERT INTO `bookings` (`id`, `booking_id`, `customer_id`, `route_id`, `customer_route`, `booked_amount`, `booked_seat`, `booking_created`) VALUES
-(60, 'TBZJ360', 'CUST-2114034', 'RT-1908653', 'CITY1 &rarr; CITY2', 100, '3', '2021-10-16 22:15:13'),
-(61, 'QK0MT61', 'CUST-2017936', 'RT-9941455', 'EDROISCHESTER &rarr; BRUGOW', 110, '15', '2021-10-17 22:36:10'),
-(62, 'A8L5662', 'CUST-5585037', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '2', '2021-10-18 00:08:51'),
-(63, 'QDNGC63', 'CUST-8996235', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '15', '2021-10-18 09:31:30'),
-(64, 'X34RW64', 'CUST-9474738', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '6', '2021-10-18 09:32:21'),
-(65, 'JKZVT65', 'CUST-4031139', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '18', '2021-10-18 09:33:36'),
-(66, 'HIIAN66', 'CUST-9997540', 'RT-5887160', 'FLORIA &rarr; ARKBY', 118, '16', '2021-10-18 09:40:16'),
-(67, 'QLOE167', 'CUST-9997540', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '12', '2021-10-18 09:41:01');
+(60, 'TBZJ360', 'CUST-2114034', 'RT-1908653', 'Bac Ninh - Ha Noi ', 60, '3', '2024-02-01 22:15:13'),
+(61, 'QK0MT61', 'CUST-2017936', 'RT-9941455', 'Ha Noi - Bac Ninh', 110, '15', '2024-01-25 22:36:10'),
+(62, 'A8L5662', 'CUST-5585037', 'RT-3835554', 'Ha Noi - Hai Duong', 70, '2', '2024-02-25 00:08:51'),
+(63, 'QDNGC63', 'CUST-8996235', 'RT-3835554', 'Ha Noi - Ho Chi Minh', 70, '15', '2024-02-24 09:31:30'),
+(64, 'X34RW64', 'CUST-9474738', 'RT-3835554', 'Ha Noi - Da Nang', 70, '6', '2024-02-22 09:32:21'),
+(65, 'JKZVT65', 'CUST-4031139', 'RT-3835554', 'Ha Noi - Ca Mau', 70, '18', '2024-02-24 09:33:36'),
+(66, 'HIIAN66', 'CUST-9997540', 'RT-5887160', 'Ha Noi - Hai Phong', 118, '16', '2024-02-14 09:40:16'),
+(67, 'QLOE167', 'CUST-9997540', 'RT-3835554', 'Ha Noi - Nha Trang', 70, '12', '2024-01-25 09:41:01'),
+(70, 'B46IR70', 'CUST-8467641', 'RT-1908653', 'HANOI &rarr; HAIPHONG', 60, '15', '2024-03-07 09:56:09');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buses`
+-- Cấu trúc bảng cho bảng `buses`
 --
 
 CREATE TABLE `buses` (
@@ -85,7 +86,7 @@ CREATE TABLE `buses` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `buses`
+-- Đang đổ dữ liệu cho bảng `buses`
 --
 
 INSERT INTO `buses` (`id`, `bus_no`, `bus_assigned`, `bus_created`) VALUES
@@ -104,7 +105,7 @@ INSERT INTO `buses` (`id`, `bus_no`, `bus_assigned`, `bus_created`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Cấu trúc bảng cho bảng `customers`
 --
 
 CREATE TABLE `customers` (
@@ -116,22 +117,23 @@ CREATE TABLE `customers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `customers`
+-- Đang đổ dữ liệu cho bảng `customers`
 --
 
 INSERT INTO `customers` (`id`, `customer_id`, `customer_name`, `customer_phone`, `customer_created`) VALUES
-(34, 'CUST-2114034', 'Dfirst Dlast', '7002001200', '2021-10-16 22:09:12'),
-(35, 'CUST-8996235', 'Willian Hobbs', '4012222222', '2021-10-17 22:30:23'),
-(36, 'CUST-2017936', 'George Watts', '7011111111', '2021-10-17 22:30:53'),
-(37, 'CUST-5585037', 'Bobb Horn', '1111111110', '2021-10-17 22:31:20'),
-(38, 'CUST-9474738', 'Alan Moore', '7900000000', '2021-10-18 09:32:02'),
-(39, 'CUST-4031139', 'Jamie Rhoades', '1003000010', '2021-10-18 09:33:08'),
-(40, 'CUST-9997540', 'Demo Customer', '7777777700', '2021-10-18 09:39:10');
+(34, 'CUST-2114034', 'Nguyen Hoai Nam', '0700200120', '2021-10-16 22:09:12'),
+(35, 'CUST-8996235', 'Nguyen Viet Hung', '0401222222', '2021-10-17 22:30:23'),
+(36, 'CUST-2017936', 'Dang Hoang Anh', '0170111111', '2021-10-17 22:30:53'),
+(37, 'CUST-5585037', 'Luu Hieu Khanh', '0111111112', '2021-10-17 22:31:20'),
+(38, 'CUST-9474738', 'Nguyen Minh Phuong', '0124578963', '2021-10-18 09:32:02'),
+(39, 'CUST-4031139', 'Ngo Thi Linh', '0987654321', '2021-10-18 09:33:08'),
+(40, 'CUST-9997540', 'Truong Huong Giang', '0123456789', '2021-10-18 09:39:10'),
+(41, 'CUST-8467641', 'Lưu  Thanh Hương', '0987654321', '2024-03-07 09:50:57');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee`
+-- Cấu trúc bảng cho bảng `employee`
 --
 
 CREATE TABLE `employee` (
@@ -151,17 +153,18 @@ CREATE TABLE `employee` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `employee`
+-- Đang đổ dữ liệu cho bảng `employee`
 --
 
 INSERT INTO `employee` (`id`, `firstName`, `lastName`, `email`, `password`, `birthday`, `gender`, `contact`, `nid`, `address`, `dept`, `degree`, `pic`) VALUES
-(2, 'John', 'Smith', 'john@gmail.com', '1234', '2020-09-01', 'Male', '0999999999', 1, 'New York', 'IT', 'Waterboy', 'images/default.jpg'),
-(6, 'test', 'test', 'test@gmail.com', '1234', '2020-09-06', 'Male', '09998383737', 4, 'test', 'test', 'test', 'images/d.jpg');
+(7, 'Giang', 'Truong Huong', 'giang@gmail.com', '1234', '2003-06-10', 'Female', '0968758462', 1, 'Ha Dong', '35', 'university', 'images/1.jpg'),
+(8, 'Linh', 'Ngo Thi', 'linh@gmail.com', '1234', '2003-10-14', 'Female', '0987654321', 2, 'Hoai Duc', '32', 'university', 'images/3.jpg'),
+(15, 'Nguyen ', 'Minh Phuong', '12345678@gmail.com', '1234', '2003-03-08', 'Female', '0987654322', 3, 'Hanoi', '32', 'university', 'images/1.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employee_leave`
+-- Cấu trúc bảng cho bảng `employee_leave`
 --
 
 CREATE TABLE `employee_leave` (
@@ -174,40 +177,16 @@ CREATE TABLE `employee_leave` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `employee_leave`
+-- Đang đổ dữ liệu cho bảng `employee_leave`
 --
 
 INSERT INTO `employee_leave` (`id`, `token`, `start`, `end`, `reason`, `status`) VALUES
-(2, 1, '2020-09-03', '2020-09-05', 'COVID-19', 'Approved'),
-(2, 3, '2020-09-10', '2020-09-12', 'May Lagnat', 'Approved');
+(7, 1, '2024-02-01', '2024-02-15', 'Tet Holiday', 'Approved');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `project`
---
-
-CREATE TABLE `project` (
-  `pid` int(11) NOT NULL,
-  `eid` int(11) DEFAULT NULL,
-  `pname` varchar(100) DEFAULT NULL,
-  `duedate` date DEFAULT NULL,
-  `subdate` date DEFAULT '0000-00-00',
-  `mark` int(11) NOT NULL,
-  `status` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `project`
---
-
-INSERT INTO `project` (`pid`, `eid`, `pname`, `duedate`, `subdate`, `mark`, `status`) VALUES
-(1, 2, 'Junkyard', '2020-09-26', '2020-09-04', 1, 'Submitted');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rank`
+-- Cấu trúc bảng cho bảng `rank`
 --
 
 CREATE TABLE `rank` (
@@ -216,17 +195,18 @@ CREATE TABLE `rank` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `rank`
+-- Đang đổ dữ liệu cho bảng `rank`
 --
 
 INSERT INTO `rank` (`eid`, `points`) VALUES
-(2, 0),
-(6, 0);
+(7, 0),
+(8, 0),
+(15, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `routes`
+-- Cấu trúc bảng cho bảng `routes`
 --
 
 CREATE TABLE `routes` (
@@ -241,23 +221,23 @@ CREATE TABLE `routes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `routes`
+-- Đang đổ dữ liệu cho bảng `routes`
 --
 
 INSERT INTO `routes` (`id`, `route_id`, `bus_no`, `route_cities`, `route_dep_date`, `route_dep_time`, `route_step_cost`, `route_created`) VALUES
-(53, 'RT-1908653', 'MVL1000', 'CITY1,CITY2', '2021-10-17', '22:05:00', 100, '2021-10-16 22:05:42'),
-(54, 'RT-3835554', 'MMM9969', 'ZEKA,ZREGOW', '2021-10-19', '23:13:00', 70, '2021-10-16 22:12:32'),
-(55, 'RT-9941455', 'RDH4255', 'EDROISCHESTER,BRUGOW', '2021-10-18', '10:00:00', 110, '2021-10-17 22:34:47'),
-(56, 'RT-9069556', 'XYZ7890', 'ANTALAND,ZREGOW', '2021-10-19', '11:40:00', 85, '2021-10-17 23:39:57'),
-(57, 'RT-775557', 'ABC0010', 'ENCEFORD,VLIRGINIA', '2021-10-19', '13:30:00', 131, '2021-10-17 23:42:12'),
-(58, 'RT-753558', 'TTH8888', 'ARKBY,VEIM', '2021-10-20', '12:04:00', 55, '2021-10-18 00:04:42'),
-(59, 'RT-6028759', 'LLL7699', 'BELRITH,ARKBY', '2021-10-20', '13:50:00', 166, '2021-10-18 00:07:50'),
-(60, 'RT-5887160', 'CAS3300', 'FLORIA,ARKBY', '2021-10-19', '10:30:00', 118, '2021-10-18 09:38:30');
+(53, 'RT-1908653', 'MVL1000', 'HAIPHONG', '2024-03-02', '22:05:00', 60, '2021-10-16 22:05:42'),
+(54, 'RT-3835554', 'MMM9969', 'NHATRANG', '2024-02-25', '23:13:00', 70, '2021-10-16 22:12:32'),
+(55, 'RT-9941455', 'RDH4255', 'DANANG ', '2024-02-27', '10:00:00', 110, '2021-10-17 22:34:47'),
+(56, 'RT-9069556', 'XYZ7890', 'CAMAU', '2024-02-20', '11:40:00', 85, '2021-10-17 23:39:57'),
+(57, 'RT-775557', 'ABC0010', 'HOCHIMINH', '2024-01-24', '13:30:00', 131, '2021-10-17 23:42:12'),
+(58, 'RT-753558', 'TTH8888', 'BACNINH', '2024-02-28', '12:04:00', 55, '2021-10-18 00:04:42'),
+(59, 'RT-6028759', 'LLL7699', 'HAIDUONG', '2024-03-01', '13:50:00', 166, '2021-10-18 00:07:50'),
+(60, 'RT-5887160', 'CAS3300', 'HANOI', '2024-02-29', '10:30:00', 118, '2021-10-18 09:38:30');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `salary`
+-- Cấu trúc bảng cho bảng `salary`
 --
 
 CREATE TABLE `salary` (
@@ -268,17 +248,18 @@ CREATE TABLE `salary` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `salary`
+-- Đang đổ dữ liệu cho bảng `salary`
 --
 
 INSERT INTO `salary` (`id`, `base`, `bonus`, `total`) VALUES
-(2, 500, 0, 500),
-(6, 1000, 0, 1000);
+(7, 10000000, 0, 10000000),
+(8, 10000000, 0, 10000000),
+(15, 1000000, 0, 1000000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `seats`
+-- Cấu trúc bảng cho bảng `seats`
 --
 
 CREATE TABLE `seats` (
@@ -287,7 +268,7 @@ CREATE TABLE `seats` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `seats`
+-- Đang đổ dữ liệu cho bảng `seats`
 --
 
 INSERT INTO `seats` (`bus_no`, `seat_booked`) VALUES
@@ -296,7 +277,7 @@ INSERT INTO `seats` (`bus_no`, `seat_booked`) VALUES
 ('CAS3300', '16'),
 ('LLL7699', NULL),
 ('MMM9969', '2,15,6,18,12'),
-('MVL1000', '3'),
+('MVL1000', '3,15'),
 ('NBS4455', NULL),
 ('RDH4255', '15'),
 ('SSX6633', NULL),
@@ -306,7 +287,7 @@ INSERT INTO `seats` (`bus_no`, `seat_booked`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `users`
 --
 
 CREATE TABLE `users` (
@@ -318,174 +299,156 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Đang đổ dữ liệu cho bảng `users`
 --
 
 INSERT INTO `users` (`user_id`, `user_fullname`, `user_name`, `user_password`, `user_created`) VALUES
-(1, 'Liam Moore', 'admin', '$2y$10$7rLSvRVyTQORapkDOqmkhetjF6H9lJHngr4hJMSM2lHObJbW5EQh6', '2021-06-02 13:55:21'),
-(2, 'Test Admin', 'testadmin', '$2y$10$A2eGOu1K1TSBqMwjrEJZg.lgy.FmCUPl/l5ugcYOXv4qKWkFEwcqS', '2021-10-17 21:10:07');
+(1, 'Linh', 'admin', '$2y$10$7rLSvRVyTQORapkDOqmkhetjF6H9lJHngr4hJMSM2lHObJbW5EQh6', '2021-06-02 13:55:21'),
+(3, 'Giang ', 'giang', 'giang@123', '2024-02-21 11:06:43'),
+(4, 'Nguyen Minh Phuong', 'PHUONG', '$2y$10$.1jIg6hZBgLluONannO8RuqKjYZSFddprZcXgeWxpt7LzZOhqsyWy', '2024-03-07 10:00:19');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `alogin`
+-- Chỉ mục cho bảng `alogin`
 --
 ALTER TABLE `alogin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `bookings`
+-- Chỉ mục cho bảng `bookings`
 --
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `buses`
+-- Chỉ mục cho bảng `buses`
 --
 ALTER TABLE `buses`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `customers`
+-- Chỉ mục cho bảng `customers`
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `employee`
+-- Chỉ mục cho bảng `employee`
 --
 ALTER TABLE `employee`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indexes for table `employee_leave`
+-- Chỉ mục cho bảng `employee_leave`
 --
 ALTER TABLE `employee_leave`
   ADD PRIMARY KEY (`token`),
   ADD KEY `employee_leave_ibfk_1` (`id`);
 
 --
--- Indexes for table `project`
---
-ALTER TABLE `project`
-  ADD PRIMARY KEY (`pid`),
-  ADD KEY `project_ibfk_1` (`eid`);
-
---
--- Indexes for table `rank`
+-- Chỉ mục cho bảng `rank`
 --
 ALTER TABLE `rank`
   ADD PRIMARY KEY (`eid`);
 
 --
--- Indexes for table `routes`
+-- Chỉ mục cho bảng `routes`
 --
 ALTER TABLE `routes`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `salary`
+-- Chỉ mục cho bảng `salary`
 --
 ALTER TABLE `salary`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `seats`
+-- Chỉ mục cho bảng `seats`
 --
 ALTER TABLE `seats`
   ADD PRIMARY KEY (`bus_no`);
 
 --
--- Indexes for table `users`
+-- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `alogin`
+-- AUTO_INCREMENT cho bảng `alogin`
 --
 ALTER TABLE `alogin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `bookings`
+-- AUTO_INCREMENT cho bảng `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
--- AUTO_INCREMENT for table `buses`
+-- AUTO_INCREMENT cho bảng `buses`
 --
 ALTER TABLE `buses`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
--- AUTO_INCREMENT for table `customers`
+-- AUTO_INCREMENT cho bảng `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
--- AUTO_INCREMENT for table `employee`
+-- AUTO_INCREMENT cho bảng `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `employee_leave`
+-- AUTO_INCREMENT cho bảng `employee_leave`
 --
 ALTER TABLE `employee_leave`
   MODIFY `token` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `project`
---
-ALTER TABLE `project`
-  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `routes`
+-- AUTO_INCREMENT cho bảng `routes`
 --
 ALTER TABLE `routes`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Constraints for dumped tables
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Constraints for table `employee_leave`
+-- Các ràng buộc cho bảng `employee_leave`
 --
 ALTER TABLE `employee_leave`
   ADD CONSTRAINT `employee_leave_ibfk_1` FOREIGN KEY (`id`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `project`
---
-ALTER TABLE `project`
-  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`eid`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `rank`
+-- Các ràng buộc cho bảng `rank`
 --
 ALTER TABLE `rank`
   ADD CONSTRAINT `rank_ibfk_1` FOREIGN KEY (`eid`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `salary`
+-- Các ràng buộc cho bảng `salary`
 --
 ALTER TABLE `salary`
   ADD CONSTRAINT `salary_ibfk_1` FOREIGN KEY (`id`) REFERENCES `employee` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
